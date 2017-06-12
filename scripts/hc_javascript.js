6fc160776a467fee3ed4c5449a2da1fb8cb512db
Hecxer <g9259279@gmail.com>
2017-06-12 13:56:03 -0500

    結合Web service之AJAX JQuery
    登入、註冊、編輯個人資料已可使用

--- scripts/hc_javascript.js
+++ scripts/hc_javascript.js
@@ -13,14 +13,17 @@ function hideSplash() {
 /********************************/
 //自我檢測
 /********************************/
+
+//檢查是否同意
+
 function isAgree() {
-    if (!($('#agreement').is(':checked'))) {
+    if (!($('#agreement').is(':checked')))
         alert("您尚未勾選同意聲明!");
-        //location.reload();
-    }
     else
         location.replace("#testSelect");
 }
+
+//計算ADL結果
 
 function calADL() {
     var sum = 0;
@@ -43,6 +46,8 @@ function calADL() {
         alert(sum + "分，您屬於完全依賴");
 }
 
+//計算GDS結果
+
 function calGDS() {
     var sum = 0;
 
@@ -62,6 +67,8 @@ function calGDS() {
         alert(sum + "分，需積極關懷及轉介專業單位");
 }
 
+//計算IADL結果
+
 function calIADL() {
     var disable = 0;
 
@@ -78,6 +85,8 @@ function calIADL() {
     else
         alert(disable + "分，正常");
 }
+
+//計算AD8結果
 
 function calAD8() {
     var sum = 0;
@@ -195,6 +204,115 @@ $(document).on("swiperight", function (e) {
 
 //Global function 通用函數
 
+
+//重設表單
+
 function resetForm() {
+    //$('#hc').trigger("reset");
+    //document.getElementsByName("hcForm").reset();
     document.getElementById("hcForm").reset();
+
 }
+
+/************************************/
+
+//登入
+
+$(function () {
+    //登入
+    var loginUrl = "http://localhost:3807/WebService1.asmx/hcLogin";
+    $('#loginBtn').click(function () {
+        //抓textBox值
+        var loginID = $('#un').val(); localStorage.loginID = $('#un').val();
+        var loginPwd = $('#pw').val(); localStorage.loginPwd = $('#pw').val();
+        $.ajax({
+            url: loginUrl,
+            type: 'POST',
+            dataType: 'xml',
+            //傳參數給WebService : {account(你在WS宣告的變數): loginID(你要傳的參數)}
+            data: { account: loginID, password: loginPwd },
+            //成功時該做的事           
+            success: function (returnXml) {
+                //WS回傳的東西是XML，要轉型態
+                var a = $('string', returnXml).text();
+                alert(a + "\n" + loginID + "\n" + loginPwd);
+                //與使用者輸入的帳號做比對，正確才跳轉頁面
+                if (loginID == a) {
+                    $.mobile.changePage("home.html", "fade");
+                }
+                else {
+                    $('#un').val(""); $('#pw').val("");
+                }
+            },
+            //失敗了該怎麼辦
+            error: function (returnXml) {
+                alert("failure");
+            }
+        })
+    });
+
+    //註冊
+    var registerUrl = "http://localhost:3807/WebService1.asmx/hcRegister";
+    $('#hcRegister').click(function () {
+        var registerID = $('#account').val(), registerPwd = $('#password').val(), registerName = $('#name').val(),
+            registerGender = $('#gender option:checked').val(), registerBirthday = $('#date').val(), registerBloodtype = $('#bldtype option:checked').val(),
+            registerEmail = $('#email').val();
+        $.ajax({
+            url: registerUrl,
+            type: 'POST',
+            dataType: 'xml',
+            data: { account: registerID, password: registerPwd, name: registerName, gender: registerGender, birthday: registerBirthday, blood_type: registerBloodtype, email: registerEmail },
+            success: function (returnXml) {
+                var registerID2 = $('string', returnXml).text();
+                alert(registerID2 + "\n" + name + " 您好!");
+                //清空所有TEXTBOX
+                $('#account').val(""); $('#password').val("");
+                $('#date').val(""); $('#name').val("");
+                $('#email').val("");
+                //if (registerID == registerID2) {
+                //    $.mobile.changePage("#popupLogin", "fade");
+                //}
+            },
+            error: function (returnXml) {
+                alert("新增失敗，請檢查網路連線。");
+            }
+        })
+    });
+
+    //顯示個人資料
+    var personalDataUrl = "http://localhost:3807/WebService1.asmx/hcPersonalData";
+    //頁面建立時觸發
+    var account = localStorage.loginID;
+    $('#pDataHomepage').on("pagecreate", function (e) {
+        //alert(e.target.id);
+        $.ajax({
+            type: 'POST',
+            url: personalDataUrl,
+            data: { account: account },
+            dataType: 'xml',
+            //oXml是自己宣告的變數
+            success: function (oXml) {
+                //因為DataSet通常是裝資料表，所以我們用下面這行來抓回傳的XML的值
+                //$(oXml).find("user").each(function () {};
+                //user是資料表名稱
+                $(oXml).find("user").each(function () {
+                    //alert("成功抓取: " + $(this).children("name").text());
+                    //$(this).children("name").text()
+                    //這行程式是抓oXml裡資料表的值，用欄位名稱來抓
+                    $('#pDataName').text($(this).children("name").text());
+                    $('#pDataEmail').text($(this).children("email").text());
+                    $('#pDataDate').text($(this).children("regis_date").text().substr(0, 10));
+                    $('#pDataGender').text($(this).children("gender").text());
+                    $('#pDataBirthday').text($(this).children("birthday").text().substr(0, 10));
+                    $('#pDataAge').text($(this).children("age").text());
+                    $('#pDataBloodtype').text($(this).children("blood_type").text());
+
+                });
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert("Failed" + xhr.status);
+                alert(thrownError);
+            }
+        });
+    });
+});
